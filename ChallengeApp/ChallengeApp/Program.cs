f2c061ba7dd7ad501498f768bbdaa2e7c3ba94ad
PatrykBur <patrykburkaczewski@gmail.com>
2025-03-08 23:40:07 +0100

    dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Witaj Patryk");
+﻿var name = "Ewa";
+var sex = "female";
+var age = 30;
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex != "female" && age < 18)
+{
+    Console.WriteLine("Niepenoletni mężczyzna");
+}
